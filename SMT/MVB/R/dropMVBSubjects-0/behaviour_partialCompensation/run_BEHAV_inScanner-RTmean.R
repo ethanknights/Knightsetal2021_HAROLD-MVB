34b7ccf58f6e1155a896545c69ed5a1b8f1aac06
ethanknights <ethanknights@hotmail.co.uk>
2021-05-11 12:21:06 +0100

    add partial compensation scripts

---
+++ SMT/MVB/R/dropMVBSubjects-0/behaviour_partialCompensation/run_BEHAV_inScanner-RTmean.R
@@ -0,0 +1,26 @@
+#======================= BEHAVIOUR IN-SCANNER ======================#
+#---- MR 1: RT ~ LH * RH ----#
+rlm_model <- rlm(inScanner_RTmean ~ univariateMean_L_0z * univariateMean_R_0z,
+                 data = df, psi = psi.huber, k = 1.345)
+summary(rlm_model)
+f.robftest(rlm_model, var="univariateMean_L_0z:univariateMean_R_0z")
+f.robftest(rlm_model)
+f.robftest(rlm_model, var="univariateMean_L_0z")
+f.robftest(rlm_model, var="univariateMean_R_0z")
+full <-           lmBF(inScanner_RTmean ~ univariateMean_L_0z * univariateMean_R_0z, data = df)
+noInteraction <-  lmBF(inScanner_RTmean ~ univariateMean_L_0z + univariateMean_R_0z, data = df)
+bf10 = full / noInteraction #less than 1 favours models of 
+bf01 = 1/bf10; bf01
+
+
+#---- MR 2: RT ~ LH * RH * age ----#
+rlm_model <- rlm(inScanner_RTmean ~ univariateMean_L_0z * univariateMean_R_0z * age0z2,
+                 data = df, psi = psi.huber, k = 1.345)
+summary(rlm_model)
+f.robftest(rlm_model)
+f.robftest(rlm_model, var=c("univariateMean_R_0z:age0z21","univariateMean_R_0z:age0z22"))
+full <-           lmBF(inScanner_RTmean ~ univariateMean_L_0z + univariateMean_R_0z * age0z, data = df)
+noInteraction <-  lmBF(inScanner_RTmean ~ univariateMean_L_0z + univariateMean_R_0z + age0z, data = df)
+bf10 = full / noInteraction #less than 1 favours models of 
+bf01 = 1/bf10; bf01
+
