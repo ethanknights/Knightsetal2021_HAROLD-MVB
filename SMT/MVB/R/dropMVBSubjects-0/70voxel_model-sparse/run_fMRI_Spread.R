741a53ec50f7d0824395819d202a24251e596351
ethanknights <ethanknights@hotmail.co.uk>
2021-07-23 17:23:03 +0100

    no real change - R mvb spread SMT

--- SMT/MVB/R/dropMVBSubjects-0/70voxel_model-sparse/run_fMRI_Spread.R
+++ SMT/MVB/R/dropMVBSubjects-0/70voxel_model-sparse/run_fMRI_Spread.R
@@ -13,8 +13,8 @@ bf01 = 1/bf10; bf01
 
 p <- ggplot(df, aes(x = age, y = spread_LH)) +
   geom_point(shape = 21, size = 3, colour = "indianred2", fill = "lightpink", stroke = 2)
-#p <- p + stat_smooth(method = "rlm", se = TRUE, fill = "grey60", formula = y ~ x, colour = "springgreen3", size = 3)
-p <- p + stat_smooth(method = "rlm", se = TRUE, fill = "grey60", formula = y ~ poly(x,2, raw = TRUE), colour = "springgreen3", size = 3)
+p <- p + stat_smooth(method = "rlm", se = TRUE, fill = "grey60", formula = y ~ x, colour = "springgreen3", size = 3)
+#p <- p + stat_smooth(method = "rlm", se = TRUE, fill = "grey60", formula = y ~ poly(x,2, raw = TRUE), colour = "springgreen3", size = 3)
 #formatting
 p <- p + 
   ylim(0,0.03) +
@@ -38,7 +38,7 @@ ggsave(file.path(outImageDir,'multivariateSpread_LH.png'),
 rlm_model <- rlm(spread_RH ~ age0z2, 
                  data = df, psi = psi.huber, k = 1.345)
 summary(rlm_model)
-f.robftest(rlm_model, var=c("age0z21","age0z22")) #age effect? 
+f.robftest(rlm_model, var=c("age0z21","age0z22")) #age effect?
 f.robftest(rlm_model, var="age0z21") #linear
 f.robftest(rlm_model, var="age0z22") #quadratic
 
@@ -48,8 +48,8 @@ bf01 = 1/bf10; bf01
 
 p <- ggplot(df, aes(x = age, y = spread_RH)) +
   geom_point(shape = 21, size = 3, colour = "indianred2", fill = "lightpink", stroke = 2)
-p <- p + stat_smooth(method = "rlm", se = TRUE, fill = "grey60", formula = y ~ x, colour = "springgreen3", size = 3)
-#p <- p + stat_smooth(method = "rlm", se = TRUE, fill = "grey60", formula = y ~ poly(x,2, raw = TRUE), colour = "springgreen3", size = 3)
+#p <- p + stat_smooth(method = "rlm", se = TRUE, fill = "grey60", formula = y ~ x, colour = "springgreen3", size = 3)
+p <- p + stat_smooth(method = "rlm", se = TRUE, fill = "grey60", formula = y ~ poly(x,2, raw = TRUE), colour = "springgreen3", size = 3)
 #formatting
 p <- p + 
   ylim(0,0.03) +
